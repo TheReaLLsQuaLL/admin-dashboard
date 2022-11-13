eb738638850f1e31eee29c0019394a584d3f589b
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-13 17:20:10 +0300

    -feat- orders page now has more pages and also added the filter feature

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -19,12 +19,29 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header title="Orders" category="Page" />
-      <GridComponent id="gridcomp" dataSource={ordersData}>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            PdfExport,
+            Edit,
+          ]}
+        />
       </GridComponent>
     </div>
   );
