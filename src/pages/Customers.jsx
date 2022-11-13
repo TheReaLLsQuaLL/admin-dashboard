0b79fc7cdd004bb53a6f69fbc9efe67c22892fdc
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-13 17:38:28 +0300

    -feat- Added the dummy data of customers to customer page also add edit and delete functions to the page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Inject,
+  Toolbar,
+  Selection,
+  Edit,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div></div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Customers" category="Page" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        width="auto"
+        toolbar={[`Delete`]}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Edit, Sort, Filter, Selection]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
