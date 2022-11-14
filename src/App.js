2a2e5910ed93da161c437cf7d41edeefeb8140ce
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-14 14:10:48 +0300

    -feat- added the settings button the functionality to expand and show theme optins but close button does not work yet need to figure out

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ import "./App.css";
 import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
   return (
     <BrowserRouter>
       <div className="flex relative dark:bg-main-dark-bg">
@@ -37,6 +37,7 @@ const App = () => {
               type="button"
               className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
               style={{ background: `blue`, borderRadius: `50%` }}
+              onClick={() => setThemeSettings(true)}
             >
               <FiSettings />
             </button>
@@ -60,7 +61,7 @@ const App = () => {
             <Navbar />
           </div>
           <div>
-            <ThemeSettings />
+            {themeSettings && <ThemeSettings />}
             <Routes>
               {/*DashBoard*/}
               <Route path="/" element={<Ecommerce />} />
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,19 @@ export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
+
+  const setColor = (e) => {
+    setCurrentColor(e.target.value);
+    localStorage.setItem("colorMode", e.target.value);
+  };
+
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem("themeMode", e.target.value);
+  };
 
   const handleClick = (clicked) => {
     console.log({ ...initialState, [clicked]: true });
@@ -29,6 +42,12 @@ export const ContextProvider = ({ children }) => {
         handleClick,
         setScreenSize,
         screenSize,
+        currentColor,
+        currentMode,
+        setCurrentColor,
+        setCurrentMode,
+        themeSettings,
+        setThemeSettings,
       }}
     >
       {children}
