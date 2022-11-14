abacb76b0d5024e8e21cf7198f7ca1a489a6396f
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-14 14:56:09 +0300

    -style- now all the coloring changes depending on the color that visitor chooses from the theme color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,6 +35,7 @@ const Navbar = () => {
     handleClick,
     screenSize,
     setScreenSize,
+    currentColor,
   } = useStateContext();
   //For sidebar sizing big and little screens
   useEffect(() => {
@@ -57,27 +58,27 @@ const Navbar = () => {
       <NavButton
         title="Menu"
         customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
-        color="blue"
+        color={currentColor}
         icon={<AiOutlineMenu />}
       />
       <div className="flex">
         <NavButton
           title="Card"
           customFunc={() => handleClick("card")}
-          color="blue"
+          color={currentColor}
           icon={<FiShoppingCart />}
         />
         <NavButton
           title="Chat"
           customFunc={() => handleClick(`chat`)}
-          color="blue"
+          color={currentColor}
           dotColor="#03C9D7"
           icon={<BsChatLeft />}
         />
         <NavButton
           title="Notifications"
           customFunc={() => handleClick(`notification`)}
-          color="blue"
+          color={currentColor}
           dotColor="#03C9D7"
           icon={<RiNotification3Line />}
         />
